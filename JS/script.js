c2ab262bd83ec24485a24882b85da1152e9bcd72
Sofia <sofia.stazi04@gmail.com>
2024-08-08 11:29:32 -0300

    Commit inicial

---
+++ JS/script.js
@@ -0,0 +1,9 @@
+function estrellitas() {
+    alert('.𖥔 ݁ ˖✧˖°.✶₊ ⊹ ⋆˚☆˖°⋆⭒˚.⋆ estrellitas ⋆⭒˚.⋆.𖥔 ݁ ˖✧˖°.✶₊ ⊹ ⋆˚☆˖°');
+};
+
+document.addEventListener('DOMContentLoaded',()=>{
+    document.getElementById('iconoContacto').addEventListener('mouseenter',()=>{
+        alert("¡Haz click en los íconos para ponernos en contacto!");
+    });
+});
