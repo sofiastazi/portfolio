06fbedc50ea0c533033660b27880d9fb0a16caf0
Sofia Stazi <sofistazi04@gmail.com>
2024-12-26 12:15:34 -0300

    add emercado and english

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,25 @@
-function estrellitas() {
-    alert('.𖥔 ݁ ˖✧˖°.✶₊ ⊹ ⋆˚☆˖°⋆⭒˚.⋆ estrellitas ⋆⭒˚.⋆.𖥔 ݁ ˖✧˖°.✶₊ ⊹ ⋆˚☆˖°');
+function stars() {
+    Swal.fire({
+        title: "Yeyyy! :D",
+        text: "You succesfully touched the stars!",
+        padding: "3em",
+        color: "#716add",
+        draggable: true,
+        backdrop: `
+          rgba(0,0,123,0.4)
+          url("/img/nyan-cat.gif")
+          left top
+          no-repeat
+        `
+      });
 };
 
 document.addEventListener('DOMContentLoaded',()=>{
-    document.getElementById('iconoContacto').addEventListener('mouseenter',()=>{
-        alert("¡Haz click en los íconos para ponernos en contacto!");
+    document.getElementById('contactIcon').addEventListener('mouseenter',()=>{
+        Swal.fire({
+            icon: "info",
+            title: "Click the icons to get in touch",
+            confirmButtonText: "Great",
+        });
     });
 });
